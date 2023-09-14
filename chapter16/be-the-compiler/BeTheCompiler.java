5f8c22518628a0c17b154960a490fd63c4810fc8
Fuad Ismail <fuad1502@gmail.com>
2023-09-14 16:51:09 +0700

    Checked chapter 16's 'Be the Compiler' solution

---
+++ chapter16/be-the-compiler/BeTheCompiler.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class BeTheCompiler {
+  public static void main(String[] args) {
+    // ArrayList<Dog> dogs1 = new ArrayList<Animal>();
+    // ArrayList<Animal> animals1 = new ArrayList<Dog>();
+    List<Animal> list = new ArrayList<Animal>();
+    ArrayList<Dog> dogs = new ArrayList<Dog>();
+    // ArrayList<Animal> animals = dogs;
+    List<Dog> dogList = dogs;
+    ArrayList<Object> objects = new ArrayList<Object>();
+    List<Object> objList = objects;
+    // ArrayList<Object> objs = new ArrayList<Dog>();
+  }
+}
