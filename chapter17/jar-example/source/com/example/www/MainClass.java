321e002724322ea9b028f0ac4f6ad0c579a90adb
Fuad Ismail <fuad1502@gmail.com>
2023-09-15 13:47:51 +0700

    Created jar-example project; added .jar file to .gitignore

---
+++ chapter17/jar-example/source/com/example/www/MainClass.java
@@ -0,0 +1,8 @@
+package com.example.www;
+
+class MainClass {
+  public static void main(String[] args) {
+    TestClass test = new TestClass();
+    test.test();
+  }
+}
---
+++ chapter17/jar-example/source/com/example/www/TestClass.java
@@ -0,0 +1,7 @@
+package com.example.www;
+
+class TestClass {
+  void test() {
+    System.out.println("Hello from TestClass!");
+  }
+}
